2de58702b1197132c95e3878bda26457f7cbb41f
Vocal <vocalnight@protonmail.com>
2024-05-19 21:06:47 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+       <nav>
+        About me
+        My Work
+        Contact
+        Download Resume
+      </nav>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
+        Hi i'm Victor, i program things and fix bugs.
+        I'm used to both front-end and back end, but really enjoy the back end side of things in systems.
+
+        Projects done:
+
+        CSharp 
+        Console projects i made while learning C#. I'm revisiting them and adding new things as i learn more about the language.
+        
+        Csharp + React
+        Csharp + Angular
+        MVC
+
+        Projects in the pipeline
+
+
+        Laravel
+        I'm learning Laravel and PHP! For that i'm building a small bugtracker for my personal use and plan to add to it over time, so this one will be in progress for a good while, but you are free to check it out.
+
+        // Cards for each project
+
+        
       </header>
+
+     
+
+
     </div>
   );
 }

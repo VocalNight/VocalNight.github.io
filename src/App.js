8c59f54a391e862c4fecc7002ed7b4e9a1158d37
Vocal <vocalnight@protonmail.com>
2024-05-19 21:46:28 -0300

    added the skeleton of each project

--- src/App.js
+++ src/App.js
@@ -18,16 +18,137 @@ function App() {
 
         CSharp 
         Console projects i made while learning C#. I'm revisiting them and adding new things as i learn more about the language.
-        
+
+        <div>
+          <div>
+            Math Game
+          </div>
+          A simple console program that tests your math knolowdge.
+        </div>
+
+        <div>
+          <div>
+            Calculator
+          </div>
+          A small calculator in C#
+        </div>
+
+        <div>
+          <div>
+            Habit Logger
+          </div>
+            Small program to track habits of your chosing.
+        </div>
+
+        <div>
+          <div>
+            Coding Tracker
+          </div>
+            Similar to the habit tracker but for coding.
+        </div>
+
+        <div>
+          <div>
+            Flashcards
+          </div>
+            A small project to achieve something similar to Anki, but in a small scale.
+        </div>
+
+        <div>
+          <div>
+            Drinks Info
+          </div>
+            A simple program to fetch data from an API.
+        </div>
+
+        <div>
+          <div>
+            Phone Book
+          </div>
+            A way to store contacts and friends information. Developed to get used to DB interactions in C#
+        </div>
+
+        <div>
+          <div>
+            Shift Logger
+          </div>
+            A more robust console project and an experiment in creating an API and consuming it. Got me interested in microservices.
+        </div>
+
+        <div>
+          <div>
+            Exercise Tracker
+          </div>
+            A more robust tracker, this time using Entity Framework, the C# ORM.
+        </div>
+
+        <div>
+          <div>
+            Sports Results Notifier
+          </div>
+            Used this one to practice how to scrap data from websties using C# features
+        </div>
+
+        <div>
+          <div>
+            Excel Reader
+          </div>
+            How to transform data from the Database into an Excel spreadsheet.
+        </div>
+
         Csharp + React
+
+        <div>
+          <div>
+            Shopping List
+          </div>
+            Small project in react to practice tables, database with back-end in C# and a bit of front end.
+        </div>
+
+        <div>
+          <div>
+            Friends Manager
+          </div>
+            Project made with tailwind for practice, also experimented with Redux.
+        </div>
+
         Csharp + Angular
+
+        <div>
+          <div>
+            Coffee Tracker
+          </div>
+            A small tracker, using tailwind with Angular for practice.
+        </div>
+
+        <div>
+          <div>
+            Sleep Tracker
+          </div>
+            A tracker using Angular material.
+        </div>
+
         MVC
+
+        <div>
+          <div>
+            Todo List
+          </div>
+            A simple Todo list to practice the ASP.NET MVC stack
+        </div>
 
         Projects in the pipeline
 
 
         Laravel
         I'm learning Laravel and PHP! For that i'm building a small bugtracker for my personal use and plan to add to it over time, so this one will be in progress for a good while, but you are free to check it out.
+
+        <div>
+          <div>
+            Laravel Bugtracker
+          </div>
+            A bugtracker made in laravel for personal use and to learn PHP.
+        </div>
 
         // Cards for each project
 

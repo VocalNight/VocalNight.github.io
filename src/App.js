7997e7781636ebe8c67dfc78230526d0c92b2722
Vocal <vocalnight@protonmail.com>
2024-06-16 23:33:56 -0300

    added contact div

--- src/App.js
+++ src/App.js
@@ -12,13 +12,15 @@ function App() {
       <nav style={{ display: 'flex', flex: 1, justifyContent: 'space-evenly' }}>
         <a href='https://github.com/VocalNight'>Github</a>
         <a href='#projects'>My Work</a>
-        <a href=''>Contact</a>
+        <a href='#contact'>Contact</a>
         <a href=''>Download Resume</a>
       </nav>
       <header className="App-header">
 
-        Hi i'm Victor, i'm a self-taught developer. I like programming things and fixing bugs.
+        Hi i'm Victor, i'm a self-taught developer from Brazil. I like programming things and fixing bugs.
         I'm used to both front-end and back end, but really enjoy the back end side of things in systems.
+
+        I also enjoy math and language learning. I'm fluent in english and know a bit of Japanese.
       </header>
 
       <div id='projects'>
@@ -44,6 +46,14 @@ function App() {
 
 
         <Accordion title="Laravel Projects" projects={laravelProjects} />
+
+      </div>
+
+      <div id='contact'>
+        Contact Me!
+
+        E-mail
+        vocalebooks@gmail.com
 
       </div>
     </div>

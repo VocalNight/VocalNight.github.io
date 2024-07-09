aaf9f12b07189d3c46f33989af49ed037de4efc2
Vocal <vocalnight@protonmail.com>
2024-07-09 17:37:27 -0300

    fixed sizing and css

--- src/App.js
+++ src/App.js
@@ -30,19 +30,17 @@ function App() {
           {projectList.length > 0 &&
           <ProjectView projects={projectList}></ProjectView>}
           <div className='headerProjects'>
-            <button className='pt-10 hover:underline' onClick={() => ShowProjects([])}>About</button>
+            <button className='pt-10 hover:underline textConfig' onClick={() => ShowProjects([])}>About</button>
             <a href={cv} download="cv.pdf">
-            <button className='hover:underline'>Download Resume</button></a>
-            <button className='hover:underline' onClick={() => ShowProjects(consoleProjects)}>C# projects</button>
-            <button className='hover:underline' onClick={() => ShowProjects(angularProjects)}>Angular projects</button>
-            <button className='hover:underline' onClick={() => ShowProjects(reactProjects)}>React Projects</button>
-            <button className='hover:underline' onClick={() => ShowProjects(laravelProjects)}>Other projects</button>
+            <button className='textConfig hover:underline'>Download Resume</button></a>
+            <button className='textConfig hover:underline' onClick={() => ShowProjects(consoleProjects)}>C# projects</button>
+            <button className='textConfig hover:underline' onClick={() => ShowProjects(angularProjects)}>Angular projects</button>
+            <button className='textConfig hover:underline' onClick={() => ShowProjects(reactProjects)}>React Projects</button>
+            <button className='textConfig hover:underline' onClick={() => ShowProjects(laravelProjects)}>Other projects</button>
           </div>
-          <div className='ContactInfo'>
-            <div>Contact me!</div>
-            <hr style={{width:"50%", marginLeft:"auto", marginRight:"auto"}}></hr>
-            <a className='hover:underline' href='https://github.com/VocalNight' target="_blank">Github</a>
-            <div>Vocalebooks@gmail.com</div>
+          <div className='ContactInfo pt-10'>
+            <a className='hover:underline textConfig' href='https://github.com/VocalNight' target="_blank">Github</a>
+            <div className='textConfig'>Vocalebooks@gmail.com</div>
           </div>
         </div>
       </div>
